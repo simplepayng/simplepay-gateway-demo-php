195bcbe9c0766ff31dc7fe0d5a7f0fea55c2e4c3
Luis Rodrigues <lfrodrigues@gmail.com>
2016-04-14 16:59:56 +0100

    Cleanup code

--- verify.php
+++ verify.php
@@ -23,10 +23,10 @@ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-    'Content-Type: application/json',                                                                                
-    'Content-Length: ' . strlen($data_string)                                                                       
-));       
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data_string)
+));
 
 $curl_response = curl_exec($ch);
 $curl_response = preg_split("/\r\n\r\n/",$curl_response);
